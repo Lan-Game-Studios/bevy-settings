b7d6cc74c516c82c83c43486696fd6b0516c9502
Tobias Kriebisch <tobias.kriebisch@tecbeast.gmbh>
2023-01-19 20:51:33 +0100

    --wip-- [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 extern crate directories;
+
 use directories::ProjectDirs;
 
 use bevy_app::{App, CoreStage, Plugin};
@@ -6,20 +7,16 @@ use bevy_ecs::{
     prelude::{EventReader, Resource},
     system::Res,
 };
+
+pub extern crate serde;
 pub use serde::{Deserialize, Serialize};
 
 const CONFIG_FILE: &str = "cookie-td.toml";
 
-pub struct SettingsPlugin<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>>(
-    pub S,
-);
+pub struct SettingsPlugin<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>>(pub S);
 
 impl<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>> SettingsPlugin<S> {
-    pub fn bootstrap() -> Self {
-        SettingsPlugin(Self::init())
-    }
-
-    pub fn init() -> S {
+    pub fn resource() -> S {
         Self::load().unwrap_or_default()
     }
 
@@ -45,24 +42,15 @@ impl<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>> Setting
     }
 }
 
-pub struct PersistEvent;
-
 impl<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>> Plugin
     for SettingsPlugin<S>
 {
     fn build(&self, app: &mut App) {
-        app.insert_resource(self.0)
+        app.insert_resource(Self::resource())
             .add_event::<PersistEvent>()
             .add_system_to_stage(CoreStage::Last, SettingsPlugin::<S>::persist);
     }
 }
 
-// fn persist(settings: Res<Settings>, mut reader: EventReader<PersistEvent>) {
-//     if reader.iter().len() > 0 {
-//         let project_dirs = ProjectDirs::from("com", "TecBeast", "Cookie TD").unwrap();
-//         let directory = project_dirs.config_dir();
-//         std::fs::create_dir_all(directory).expect("Couldn't write a configuration file");
-//         let path = (*directory).join(CONFIG_FILE);
-//         std::fs::write(path, toml::to_string(&*settings).unwrap()).unwrap();
-//     }
-// }
+pub struct PersistEvent;
+
