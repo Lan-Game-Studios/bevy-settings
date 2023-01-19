d624f31a03a15bf2dada33af237a11ac725ed8fa
Tobias Kriebisch <tobias.kriebisch@tecbeast.gmbh>
2023-01-19 20:38:18 +0100

    its compiling

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,68 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+extern crate directories;
+use directories::ProjectDirs;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use bevy_app::{App, CoreStage, Plugin};
+use bevy_ecs::{
+    prelude::{EventReader, Resource},
+    system::Res,
+};
+pub use serde::{Deserialize, Serialize};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+const CONFIG_FILE: &str = "cookie-td.toml";
+
+pub struct SettingsPlugin<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>>(
+    pub S,
+);
+
+impl<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>> SettingsPlugin<S> {
+    pub fn bootstrap() -> Self {
+        SettingsPlugin(Self::init())
+    }
+
+    pub fn init() -> S {
+        Self::load().unwrap_or_default()
+    }
+
+    fn load() -> Option<S> {
+        let path = ProjectDirs::from("com", "TecBeast", "Cookie TD")?
+            .config_dir()
+            .join(CONFIG_FILE);
+        if !path.exists() {
+            return Some(S::default());
+        }
+        let settings_string = std::fs::read_to_string(path).ok()?;
+        toml::from_str(&settings_string).ok()
+    }
+
+    fn persist(settings: Res<S>, mut reader: EventReader<PersistEvent>) {
+        if reader.iter().len() > 0 {
+            let project_dirs = ProjectDirs::from("com", "TecBeast", "Cookie TD").unwrap();
+            let directory = project_dirs.config_dir();
+            std::fs::create_dir_all(directory).expect("Couldn't write a configuration file");
+            let path = (*directory).join(CONFIG_FILE);
+            std::fs::write(path, toml::to_string(&*settings).unwrap()).unwrap();
+        }
     }
 }
+
+pub struct PersistEvent;
+
+impl<S: Resource + Copy + Serialize + Default + for<'a> Deserialize<'a>> Plugin
+    for SettingsPlugin<S>
+{
+    fn build(&self, app: &mut App) {
+        app.insert_resource(self.0)
+            .add_event::<PersistEvent>()
+            .add_system_to_stage(CoreStage::Last, SettingsPlugin::<S>::persist);
+    }
+}
+
+// fn persist(settings: Res<Settings>, mut reader: EventReader<PersistEvent>) {
+//     if reader.iter().len() > 0 {
+//         let project_dirs = ProjectDirs::from("com", "TecBeast", "Cookie TD").unwrap();
+//         let directory = project_dirs.config_dir();
+//         std::fs::create_dir_all(directory).expect("Couldn't write a configuration file");
+//         let path = (*directory).join(CONFIG_FILE);
+//         std::fs::write(path, toml::to_string(&*settings).unwrap()).unwrap();
+//     }
+// }
